09c2fd29994085a0399c1fcc42d0363e054f74e2
Mohammadreza <visapick.it7@gmail.com>
2023-10-25 15:59:09 +0330

    refactor: update models

--- iam/schema.py
+++ iam/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from uuid import UUID
 from typing import Optional, List
 
@@ -15,18 +15,34 @@ class TokenBasePayload(BaseModel):
 
     allowed_origins: Optional[list[str]] = Field(..., alias="allowed-origins")
     realm_access: RealmAccess = RealmAccess(roles=[])
+    scope: Optional[str] = None
+    groups: Optional[list[str]] = []
+    preferred_username: Optional[str] = None
+
+    @property
+    def scopes(self) -> list[str]:
+        return self.scope.split(" ")
+    
+    # @field_validator("scopes")
+    # @classmethod
+    # def str_scopes_to_list(cls, v: str) -> list[str]:
+    #     if not v:
+    #         raise ValueError("must contain a space")
+    #     return v.split(" ")
 
 
 class UserAttributes(TokenBasePayload):
-    id: Optional[UUID]
-    given_name: Optional[str]
-    email_verified: Optional[bool]
-    preferred_username: Optional[str]
-    given_name: Optional[str]
-    family_name: Optional[str]
-    email: Optional[str]
-    name: Optional[str]
-    scopes: list[str]
+    id: Optional[UUID] = None
+    given_name: Optional[str] = None
+    email_verified: Optional[bool] = None
+    given_name: Optional[str] = None
+    family_name: Optional[str] = None
+    email: Optional[str] = None
+    name: Optional[str] = None
+
+
+class CleintAttributes(TokenBasePayload):
+    clientId: Optional[str] = None
 
 
 class TokenPayload(UserAttributes):
